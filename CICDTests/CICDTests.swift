22e9ea8c9e127c5bb2d2a9121a9def243c686304
Khawaja-Abdul-Haleem-ios <abdul.haleem@hypernymbiz.com>
2025-04-29 17:08:22 +0500

    first commit

---
+++ CICDTests/CICDTests.swift
@@ -0,0 +1,17 @@
+//
+//  CICDTests.swift
+//  CICDTests
+//
+//  Created by Khawaja Abdul Haleem on 29/04/2025.
+//
+
+import Testing
+@testable import CICD
+
+struct CICDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
